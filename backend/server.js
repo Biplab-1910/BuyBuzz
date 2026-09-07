a87d189f206d62910d233ab3f0c63a3b2f81c661
Biplab <mahatobiplab777@gmail.com>
2026-09-08 02:12:46 +0530

    Fix backend CORS

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,11 @@ connectDB();
 // MIDDLEWARE
 
 
-app.use(cors());
+app.use(cors({
+  origin: "https://buybuzz.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
 
